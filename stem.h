2b0393b8509970687621993f28da0a3594be69f0
Nix3l <momanianas123@gmail.com>
2025-01-11 15:49:26 +0300

    changed up a few things

--- stem.h
+++ stem.h
@@ -14,19 +14,19 @@
 //       thank you arduino IDE devs very cool
 
 // MACROS
-#define MIN_TURN_DIST (6.0f)
-#define MIN_WALL_DIST (6.0f)
+#define MIN_TURN_DIST (5.0f)
+#define MIN_WALL_DIST (3.0f)
 
 // PINS
 // NOTE(anas): right motor -> A
 //             left  motor -> B
-#define MOTOR_RIGHT_ENABLE  (7)
-#define MOTOR_LEFT_ENABLE   (2)
+#define MOTOR_RIGHT_ENABLE  (6)
+#define MOTOR_LEFT_ENABLE   (3)
 
-#define MOTOR_RIGHT_IN1     (5)
-#define MOTOR_RIGHT_IN2     (6)
+#define MOTOR_RIGHT_IN1     (7)
+#define MOTOR_RIGHT_IN2     (5)
 #define MOTOR_LEFT_IN1      (4)
-#define MOTOR_LEFT_IN2      (3)
+#define MOTOR_LEFT_IN2      (2)
 
 #define US_LEFT_TRIGGER     (13)
 #define US_LEFT_ECHO        (12)
@@ -37,7 +37,7 @@
 #define US_RIGHT_TRIGGER    (9)
 #define US_RIGHT_ECHO       (8)
 
-#define IR_IN               (22)
+#define MOTOR_K             (35)
 
 // DATA TYPES
 typedef char i8;
@@ -51,23 +51,6 @@ typedef enum {
     ROT_CW  = 0,
     ROT_ACW = 1,
 } rotation_t;
-
-// NOTE(anas): directions are defined in clockwise order
-//             which lets us do some tricks with for loops
-//             and arithmetic that makes handling rotations easier
-typedef enum {
-    DIR_FORWARD = 0,
-    DIR_RIGHT   = 1,
-    DIR_BACK    = 2,
-    DIR_LEFT    = 3,
-} dir_t;
-
-typedef enum {
-    DIR_NORTH   = 0,
-    DIR_EAST    = 1,
-    DIR_SOUTH   = 2,
-    DIR_WEST    = 3,
-} cardinal_t;
 
 typedef enum {
     MOVE_STILL      = 0,
@@ -84,6 +67,7 @@ typedef struct {
     u16 in2;
 
     boolean running;
+    u8 speed;
     rotation_t rot;
 } motor_s;
 
@@ -94,27 +78,18 @@ typedef struct {
     f32 dist;
 } ultrasonic_s;
 
-typedef struct {
-    u16 pin;
-    u8 val; // 0 when white object
-            // 1 when black object
-} ir_s;
-
 // ROBOT
 typedef struct {
     ultrasonic_s us_left;
     ultrasonic_s us_front;
     ultrasonic_s us_right;
 
-    ir_s ir;
-
     motor_s motor_right;
     motor_s motor_left;
 
-    u8 speed;     // speed when moving forward/backward
-    u8 rot_speed; // speed when turning
-
-    cardinal_t dir;
+    u8 speed;
+    u8 parent_rot_speed; 
+    u8 child_rot_speed;
 
     boolean turning;
     movement_t movement;
